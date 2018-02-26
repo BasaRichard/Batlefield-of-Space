782034f3f2d97766218e904301d8f95774f349a0
Ricsi <bricharddavid@gmail.com>
2018-02-26 13:48:38 +0100

    Bugos csproj2

--- Batlefield of Space/Batlefield_of_Space/Program.cs
+++ Batlefield of Space/Batlefield_of_Space/Program.cs
@@ -10,7 +10,7 @@ namespace Batlefield_of_Space
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (GAME game = new GAME())
             {
                 game.Run();
             }
---
+++ Batlefield of Space/Batlefield_of_Space/Ship/Spaceship.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Batlefield_of_Space
+{
+   abstract class Spaceship
+    {
+        int Healt;
+        int Speed;
+        int basedamage;
+        public Texture2D Texture { get; set; }
+        private Rectangle position;
+
+        //asd
+
+
+    }
+}
