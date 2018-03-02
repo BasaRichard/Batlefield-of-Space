95fffd7228baf7d4857eee88efd6b175ee5ec3ca
Suli <Suli@gt209-453>
2018-03-02 14:22:19 +0100

    hajoképek + link

--- Batlefield of Space/Batlefield_of_Space/Ship/Spaceship.cs
+++ Batlefield of Space/Batlefield_of_Space/Ship/Spaceship.cs
@@ -17,6 +17,17 @@ namespace Batlefield_of_Space
 
         //asd
 
+//        ship :
+//1. https://opengameart.org/content/spaceship-fighter
+//2. https://opengameart.org/content/2d-spaceship-7-wubitog
+//3. https://www.pinterest.co.uk/pin/563442603351883947/
+//4. https://www.pinterest.co.uk/pin/563442603351883951/
+//5. https://opengameart.org/content/2d-spaceship-10
+//6. https://opengameart.org/content/2d-spaceship-10
+//7. https://opengameart.org/content/spaceship-fighter-ipod1
+//8.
+//9.
+//10.
 
     }
 }
